21d83b189209192942da58e9b6143935945f51e9
xalava <xavier@bricodeur.com>
2023-12-01 08:23:33 +0200

    extract text

---
+++ extract-text.sh
@@ -0,0 +1,21 @@
+
+#!/bin/bash
+
+word="digitalization"
+
+for folder in ./TA/*; do
+  if [ -d "$folder" ]; then
+    echo "## $folder ($(ls -1 $folder |wc -l) reports):"
+    # Loop through files within each folder
+    for file in "$folder"/*; do
+      if [ -f "$file" ]; then
+        # echo "Processing file: $file"
+				# i ignore case, H print filename, n page number
+        pdfgrep -in $word $file && echo "[From $(pdfinfo $file | head -n 1)]"
+        # pdfgrep -iHn --with-filename --color always 'digitaliz' $file && pdfinfo $file | head -n 1
+        
+      fi
+    done
+    
+  fi
+done
